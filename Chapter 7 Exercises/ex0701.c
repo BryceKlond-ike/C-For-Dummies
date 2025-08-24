f651496c96f6375d53aadd7b5d537b25651913f1
BryceKlond-ike <klabundebryce@gmail.com>
2025-08-24 16:57:58 -0400

    Finished out chapter 7 exercises.

--- Chapter 7 Exercises/ex0701.c
+++ Chapter 7 Exercises/ex0701.c
@@ -171,8 +171,8 @@ int main() {
 }
 */
 
-// Exercise 7-13. First, and last name, get intput via scanf(), display output, first+last.
-
+/* Exercise 7-13. First, and last name, get intput via scanf(), display output, first+last.
+                // Bruno saw this code. cool moment. 08/24/2025
 int main() {
 
     char firstName[15], lastName[15];
@@ -190,3 +190,99 @@ int main() {
     return(0);
 
 }
+*/
+
+/* Exercise 7-14, Listing 7-6. scanf() eats an integer. Build, run, test. 
+
+int main() {
+
+    int fav;
+    
+    printf("What is your favorite number? \n");
+    scanf("%d", &fav); // need & (address-of operator) with any variable (array execption) and scanf().
+    
+    printf("%d is my favorite number, too! \n", fav);
+    return(0);
+
+    // works as intended with integers, duh, floats lose their tails, any other character returns 32758. 
+}
+*/
+
+/* Exercise 7-15, Modify L7-6 to use floating point numbers, mind conversion. 
+int main() {
+
+    float fav;
+    
+    printf("What is your favorite number? \n");
+    scanf("%f", &fav); // need & (address-of operator) with any variable (array execption) and scanf().
+    
+    printf("%f is my favorite number, too! \n", fav);
+    return(0);
+    // w/o limitations, returns in 8-digit format. Works though.
+}
+*/
+
+/* Listing 7-7, Exercise 7-16 introducing fgets(string,size,stdin).
+
+int main () {
+
+    char name[15]; // name = string 
+
+    printf("What do they call you? \n");
+    fgets(name, 15, stdin);
+
+    printf("The bell rings for you, %s Muah haha haah \n", name); // almost halloween. 
+    return(0);
+    // seems to read white space as spaces, so can shift formatting if not spot one to what its expecting. 
+
+}
+*/
+
+/* Exercise 7-17. Change array size to a const value. Set const to only allow three characters input. 
+// added my own spin from modern memery ;)
+int main() {
+
+    char name[3]; // doesn't accept variables lmao;
+
+    printf("\"What's ya name?\", The man across the street yells out to you. \n");
+    
+    fgets(name, 3, stdin);
+    printf("\"%s\", you yell back, unsure and slightly perturbed.\n", name);
+    getchar();
+
+    printf("What? \n");
+    getchar();
+
+    printf("He doesn't to hear your last attempt to communicate outside his pizza shop, over the busy NYC traffic.\n");
+    getchar();
+
+    printf("Aww well, to #$!@ with you anyways, %s.", name);
+    getchar();
+    return(0);
+
+    // fgets size limitation is always -1. I wonder how to format the white space though?
+}
+*/
+
+// Exercise 7-18, redo 7-13 with fgets() instead of scanf() to read in the two strings. 
+/*
+int main() {
+
+    char firstName[15], lastName[15];
+
+    printf("Howdy partner, please enter your first name: \n");
+    fgets(firstName, 15, stdin);
+    
+
+    printf("Now, please enter your last name: \n");
+    fgets(lastName, 15, stdin);
+    
+
+    printf("Thank you! Good to meet you %s %s \n", firstName, lastName);
+    getchar();
+    return(0);
+    // Works as intended, though whitespace formatting is annoying with fgets()
+}
+*/
+
+// Carry on in chapter 8!
