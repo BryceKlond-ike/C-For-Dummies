e644dfa6d74ba77714f38ef8795936a92361c088
BryceKlond-ike <klabundebryce@gmail.com>
2025-09-04 11:25:30 -0400

    Finished out Chapter 9 Exercises. All on Loops.

--- Chapter 9 Exercises/ex0901.c
+++ Chapter 9 Exercises/ex0901.c
@@ -150,7 +150,7 @@ int main() {
 }
 //*/
 
-///* Listing 9-5 A Nested Loop. Exercise 9-11, Build and run.
+/* Listing 9-5 A Nested Loop. Exercise 9-11, Build and run.
 
 int main() {
 	
@@ -167,3 +167,181 @@ int main() {
 
 }
 //*/
+
+/* Exercise 9-12, 3 Letter acronym AAA to ZZZ.
+int main() {
+
+	char alpha, beta, zulu;
+
+	for(alpha='A'; alpha<='Z'; alpha=alpha+1) {
+		for(beta='A'; beta<='Z'; beta=beta+1) {
+			for(zulu='A'; zulu<='Z'; zulu=zulu+1) {
+				printf("%c%c%c\t", alpha,beta,zulu);
+			}
+		}
+	}
+	putchar('\n');
+	return(0);
+}
+//*/
+
+/* Listing 9-6, exercise 9-13 Intro to while() loop. 
+
+int main() {
+
+	int x;
+
+	x=0;
+	while(x<10) {
+		puts("Sore Shoulder Surgery");
+		x=x+1;
+	}
+	putchar('\n');
+	return(0);
+
+}
+//*/
+
+/* Exercise 9-14. changing value of x
+
+int main() {
+
+	int x;
+
+	x=13;
+	while(x<10) {
+		puts("Sore shoulder surgery");
+		x=x+1;
+	}
+	return(0);
+	// Code will build, but there is no output because the value is already true before loop starts, therefore loop doesnt run. 
+}
+*/
+
+/* Exercise 9-15, from -5 to 5, a while loop
+
+int main() {
+	float h;
+
+	h=-5.0;
+	while(h<5) {
+		printf("%.1f\t", h); // [tab] for readability, naturally
+		h=h+0.5;
+	}
+	putchar('\n');
+	return(0);
+}
+*/
+
+/* Exercise 9-16, Listing 9-7, do-while() loop intro.
+
+int main() {
+
+	int fibo, nacci;
+
+	fibo=0;
+	nacci=1;
+
+	do{
+		printf("%d ", fibo);
+		fibo=fibo+nacci;
+
+		printf("%d ", nacci);
+		nacci=nacci+fibo;
+	} while(nacci < 300);
+
+	putchar('\n');
+	return(0);
+}
+*/
+
+/* Exercise 9-17, redo 9-14 as a do-while() loop. 
+
+int main() {
+	
+	int x;
+
+	x=0;
+	do {
+		puts("Sore shoulder surgery");
+		x=x+1;
+	} while(x<10);
+	return(0);
+}
+*/
+
+/* Exercise 9-18, Listing 9-8 Common endless loops.
+
+int main() {
+
+	int x;
+
+	for(x=0;x=10;x=x+1) {
+		puts("What are you looking at?");
+	}
+	return(0);
+	// Endless loop
+}
+*/
+
+/* Exercise 9-19, Listing 9-9. Breaking out of a loop!
+
+int main() {
+
+	int count;
+
+	count = 0;
+	while(1) {
+		printf("%d, ", count);
+		count=count+1;
+		if(count>50)
+			break;
+	}
+	putchar('\n');
+	return(0);
+}
+*/
+
+/* Exercise 9-20, rewrite Listing 9-9 using endless for() loop. 
+
+int main() {
+
+	int count;
+	for(count=0;;count=count+1) {
+		printf("%d, ", count);
+		if(count>=50) {
+			break;
+		}
+	}
+	putchar('\n');
+	return(0);
+}
+*/
+
+/* Lisitng 9-10. Exercise 9-21, build and run. Adding multiple for() loop conditions
+
+int main() {
+
+	int a;
+	char c;
+
+	for(a=1, c='Z'; a < 5; a=a+1, c=c-1) {
+		printf("%d%c\n", a,c);		//Creates Alpha numeric code
+	}
+	return(0);
+
+}
+*/
+
+/* Exercise 9-22, listing 9-11. Stuffing a loop.
+
+int main() {
+
+	int x;
+
+	for(x=0; x<10; x=x+1, printf("%d\n", x))
+		;
+	return(0);
+	//While it definitely works, bad practice.
+}
+*/
